33af2f7ff60977e7f821f297772b5df903e9418c
Bukosabino <bukosabino@gmail.com>
2018-04-05 12:20:22 +0100

    wrapper

--- wrapper.py
+++ wrapper.py
@@ -16,13 +16,13 @@ def add_volume_ta(df, high, low, close, volume):
     Returns:
         pandas.core.frame.DataFrame: Dataframe with new features.
     """
-    df['v1'] = acc_dist_index(df[high], df[low], df[close], df[volume])
-    df['v2'] = on_balance_volume(df[close], df[volume])
-    df['v3'] = on_balance_volume_mean(df[close], df[volume], 10)
-    df['v5'] = chaikin_money_flow(df[high], df[low], df[close], df[volume])
-    df['v6'] = force_index(df[close], df[volume])
-    df['v7'] = ease_of_movement(df[high], df[low], df[close], df[volume], 14)
-    df['v8'] = volume_price_trend(df[close], df[volume])
+    df['volume1'] = acc_dist_index(df[high], df[low], df[close], df[volume])
+    df['volume2'] = on_balance_volume(df[close], df[volume])
+    df['volume3'] = on_balance_volume_mean(df[close], df[volume], 10)
+    df['volume5'] = chaikin_money_flow(df[high], df[low], df[close], df[volume])
+    df['volume6'] = force_index(df[close], df[volume])
+    df['volume7'] = ease_of_movement(df[high], df[low], df[close], df[volume], 14)
+    df['volume8'] = volume_price_trend(df[close], df[volume])
     return df
 
 
@@ -38,17 +38,17 @@ def add_volatility_ta(df, high, low, close):
     Returns:
         pandas.core.frame.DataFrame: Dataframe with new features.
     """
-    df['vo1'] = average_true_range(df[high], df[low], df[close], n=14)
-    df['vo2'] = bollinger_hband(df[close], n=20, ndev=2)
-    df['vo3'] = bollinger_lband(df[close], n=20, ndev=2)
-    df['vo4'] = bollinger_mavg(df[close], n=20)
-    df['vo5'] = bollinger_hband_indicator(df[close], n=20, ndev=2)
-    df['vo6'] = bollinger_lband_indicator(df[close], n=20, ndev=2)
-    df['vo7'] = keltner_channel(df[high], df[low], df[close], n=10)
-    df['vo8'] = donchian_channel_hband(df[close], n=20)
-    df['vo9'] = donchian_channel_lband(df[close], n=20)
-    df['vo10'] = donchian_channel_hband_indicator(df[close], n=20)
-    df['vo11'] = donchian_channel_lband_indicator(df[close], n=20)    
+    df['volatility1'] = average_true_range(df[high], df[low], df[close], n=14)
+    df['volatility2'] = bollinger_hband(df[close], n=20, ndev=2)
+    df['volatility3'] = bollinger_lband(df[close], n=20, ndev=2)
+    df['volatility4'] = bollinger_mavg(df[close], n=20)
+    df['volatility5'] = bollinger_hband_indicator(df[close], n=20, ndev=2)
+    df['volatility6'] = bollinger_lband_indicator(df[close], n=20, ndev=2)
+    df['volatility7'] = keltner_channel(df[high], df[low], df[close], n=10)
+    df['volatility8'] = donchian_channel_hband(df[close], n=20)
+    df['volatility9'] = donchian_channel_lband(df[close], n=20)
+    df['volatility10'] = donchian_channel_hband_indicator(df[close], n=20)
+    df['volatility11'] = donchian_channel_lband_indicator(df[close], n=20)    
     return df
 
 
