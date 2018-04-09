a40dfb7fe942a6cf2d7dbc87cadfa629179b8dfd
Bukosabino <bukosabino@gmail.com>
2018-04-09 17:39:51 +0100

    update pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 setup(
     name = 'ta',
     packages = ['ta'],
-    version = '0.1.3',
+    version = '0.1.4',
     description='Technical Analysis Library in Python',
+    long_description='It is a technical analysis library to financial time series datasets. You can use to do feature engineering. It is builded on pandas python library.',
     author = 'Dario Lopez Padial (Bukosabino)',
     author_email = 'bukosabino@gmail.com',
     url = 'https://github.com/bukosabino/ta',
@@ -13,7 +14,7 @@ setup(
         'numpy',
         'pandas'
     ],
-    download_url = 'https://github.com/bukosabino/ta/tarball/0.1.3',
+    download_url = 'https://github.com/bukosabino/ta/tarball/0.1.4',
     keywords = ['technical analysis', 'python3', 'pandas', 'numpy',
                 'fundamental analysis'],
     license='The MIT License (MIT)',
