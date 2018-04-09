d753ddc5b53bf50fc0d7284532928cb207a2d71f
Bukosabino <bukosabino@gmail.com>
2018-04-10 00:28:41 +0100

    documentation to dev

---
+++ dev/generate_image_macd.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+from ta import *
+
+# Load data
+df = pd.read_csv('../data/datas.csv', sep=',')
+df = utils.dropna(df)
+
+# Add all ta features filling nans values
+df = add_all_ta_features(df, "Open", "High", "Low", "Close", "Volume_BTC",
+                            fillna=True)
+# Generate macd image
+plt.plot(df[40500:41000].trend1, label='MACD')
+plt.plot(df[40500:41000].trend2, label='MACD Signal')
+plt.plot(df[40500:41000].trend3, label='MACD Difference')
+plt.title('MACD, MACD Signal and MACD Difference')
+plt.legend()
+plt.savefig("macd.png")
