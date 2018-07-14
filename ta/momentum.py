d778f1b8e53028b70a396e51cbbda40ec219d56c
coire <coire.cadeau@gmail.com>
2018-07-14 19:32:35 +0100

    Adding William R% and Awesome Oscillator to momentum.py, and calls in wrapper.py.

--- ta/momentum.py
+++ ta/momentum.py
@@ -135,9 +135,9 @@ def uo(high, low, close, s=7, m=14, l=28, ws=4.0, wm=2.0, wl=1.0, fillna=False):
         s(int): short period
         m(int): medium period
         l(int): long period
-        ws(float): weight of short BP average for UO
-        wm(float): weight of medium BP average for UO
-        wl(float): weight of long BP average for UO
+        ws(float): weight of short period average for UO
+        wm(float): weight of medium period average for UO
+        wl(float): weight of long period average for UO
         fillna(bool): if True, fill nan values with 50.
 
 
@@ -165,6 +165,79 @@ def uo(high, low, close, s=7, m=14, l=28, ws=4.0, wm=2.0, wl=1.0, fillna=False):
         uo = uo.fillna(50)
     return pd.Series(uo, name='uo')
 
+def wr(high, low, close, lbp=14, fillna=False):
+    """Williams %R
+
+    From: http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:williams_r
+    Developed by Larry Williams, Williams %R is a momentum indicator that is the inverse of the 
+    Fast Stochastic Oscillator. Also referred to as %R, Williams %R reflects the level of the close 
+    relative to the highest high for the look-back period. In contrast, the Stochastic Oscillator 
+    reflects the level of the close relative to the lowest low. %R corrects for the inversion by
+    multiplying the raw value by -100. As a result, the Fast Stochastic Oscillator and Williams %R 
+    produce the exact same lines, only the scaling is different. Williams %R oscillates from 0 to -100. 
+    
+    Readings from 0 to -20 are considered overbought. Readings from -80 to -100 are considered oversold. 
+    
+    Unsurprisingly, signals derived from the Stochastic Oscillator are also applicable to Williams %R.
+
+
+    %R = (Highest High - Close)/(Highest High - Lowest Low) * -100
+   
+    Lowest Low = lowest low for the look-back period
+    Highest High = highest high for the look-back period
+    %R is multiplied by -100 correct the inversion and move the decimal.
+
+    From: https://www.investopedia.com/terms/w/williamsr.asp
+    The Williams %R oscillates from 0 to -100. When the indicator produces readings from 0 to -20, this indicates 
+    overbought market conditions. When readings are -80 to -100, it indicates oversold market conditions.
+
+
+    Args:
+        high(pandas.Series): dataset 'High' column.
+        low(pandas.Series): dataset 'Low' column.
+        close(pandas.Series): dataset 'Close' column.
+        lbp(int): lookback period
+        fillna(bool): if True, fill nan values with -50.
+    """
+
+    hh=high.rolling(lbp).max() #highest high over lookback period lbp
+    ll=low.rolling(lbp).min()  #lowest low over lookback period lbp
+
+    wr = -100*(hh-close)/(hh-ll)
+
+    if fillna:
+        wr = wr.fillna(-50)
+    return pd.Series(wr, name='wr')
+
+
+def ao(high, low, s=5, l=34, fillna=False):
+    """Awesome Oscillator
+
+    From: https://www.tradingview.com/wiki/Awesome_Oscillator_(AO)
+    The Awesome Oscillator is an indicator used to measure market momentum. AO calculates the difference of a 
+    34 Period and 5 Period Simple Moving Averages. The Simple Moving Averages that are used are not calculated 
+    using closing price but rather each bar's midpoints. AO is generally used to affirm trends or to anticipate 
+    possible reversals. 
+
+    
+    From: https://www.ifcm.co.uk/ntx-indicators/awesome-oscillator
+
+    Awesome Oscillator is a 34-period simple moving average, plotted through the central points of the bars (H+L)/2, 
+    and subtracted from the 5-period simple moving average, graphed across the central points of the bars (H+L)/2.
+    MEDIAN PRICE = (HIGH+LOW)/2
+    AO = SMA(MEDIAN PRICE, 5)-SMA(MEDIAN PRICE, 34)
+
+    where
+
+    SMA — Simple Moving Average.
+    """
+
+    mp = (high+low)/2.0
+    ao = mp.rolling(s).mean()-mp.rolling(l).mean()
+
+    if fillna:
+        ao = ao.fillna(0)
+    return pd.Series(ao, name='ao')
 
 # TODO:
-# Stochastic oscillator / Williams %R (%R)
+# what else?
