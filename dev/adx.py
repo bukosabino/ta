ab2a29d804170ad777d6658bcc379d3ae2827ad0
Bukosabino <bukosabino@gmail.com>
2018-11-03 11:41:24 +0100

    dev

--- dev/adx.py
+++ dev/adx.py
@@ -5,8 +5,8 @@ sys.path.append("..") # Adds higher directory to python modules path.
 from ta import *
 
 # Load data
-#df = pd.read_csv('../data/datas2.csv', sep=',')
-df = pd.read_csv('prueba.csv', sep=',')
+df = pd.read_csv('../data/datas.csv', sep=',')
+# df = pd.read_csv('prueba.csv', sep=',')
 
 # Clean nan values
 df = utils.dropna(df)
@@ -22,3 +22,4 @@ df['adx_neg'] = adx_neg(df['High'], df['Low'], df['Close'], n=14, fillna=True)
 df['adx_pos'] = adx_pos(df['High'], df['Low'], df['Close'], n=14, fillna=True)
 
 print(df.columns)
+print(df.tail())
