651a4f9ff4003df48610fee37f9020857993bbed
Dario Lopez Padial <bukosabino@gmail.com>
2019-04-13 10:22:22 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'ta',
     packages = ['ta'],
-    version = '0.4.0',
+    version = '0.4.2',
     description='Technical Analysis Library in Python',
     long_description='It is a Technical Analysis library to financial time series datasets. You can use to do feature engineering. It is builded on Python Pandas library.',
     author = 'Dario Lopez Padial (Bukosabino)',
@@ -12,7 +12,7 @@ setup(
     maintainer='Dario Lopez Padial (Bukosabino)',
     maintainer_email='Bukosabino@gmail.com',
     install_requires=open('requirements.txt').readlines(),
-    download_url = 'https://github.com/bukosabino/ta/tarball/0.4.0',
+    download_url = 'https://github.com/bukosabino/ta/tarball/0.4.2',
     keywords = ['technical analysis', 'python3', 'pandas'],
     license='The MIT License (MIT)',
     classifiers = [],
