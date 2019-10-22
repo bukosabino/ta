5b5ff6820faa759c47d2df16417aea4d28f6bda2
Dario Lopez Padial <bukosabino@gmail.com>
2019-10-22 09:01:33 +0200

    adding roc indicator: https://github.com/bukosabino/ta/issues/78

--- ta/momentum.py
+++ ta/momentum.py
@@ -379,3 +379,36 @@ def kama(close, n=10, pow1=2, pow2=30, fillna=False):
         kama = kama.replace([np.inf, -np.inf], np.nan).fillna(close)
 
     return kama
+
+
+def roc(close, n=12, fillna=False):
+    """Rate of Change (ROC)
+
+    The Rate-of-Change (ROC) indicator, which is also referred to as simply
+    Momentum, is a pure momentum oscillator that measures the percent change in
+    price from one period to the next. The ROC calculation compares the current
+    price with the price “n” periods ago. The plot forms an oscillator that
+    fluctuates above and below the zero line as the Rate-of-Change moves from
+    positive to negative. As a momentum oscillator, ROC signals include
+    centerline crossovers, divergences and overbought-oversold readings.
+    Divergences fail to foreshadow reversals more often than not, so this
+    article will forgo a detailed discussion on them. Even though centerline
+    crossovers are prone to whipsaw, especially short-term, these crossovers
+    can be used to identify the overall trend. Identifying overbought or
+    oversold extremes comes naturally to the Rate-of-Change oscillator.
+
+    https://school.stockcharts.com/doku.php?id=technical_indicators:rate_of_change_roc_and_momentum
+
+    Args:
+        close(pandas.Series): dataset 'Close' column.
+        n(int): n periods.
+        fillna(bool): if True, fill nan values.
+
+    Returns:
+        pandas.Series: New feature generated.
+
+    """
+    roc = ((close - close.shift(n)) / close.shift(n)) * 100
+    if fillna:
+        roc = roc.replace([np.inf, -np.inf], np.nan).fillna(0)
+    return pd.Series(roc, name='roc')
