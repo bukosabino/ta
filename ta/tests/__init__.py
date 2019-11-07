b6f71b234f0cf469561346595ff81fdd674079f2
Dario Lopez Padial <bukosabino@gmail.com>
2019-11-07 23:27:09 +0100

    fixing ease of movement implementation and adding test: https://github.com/bukosabino/ta/issues/58

--- ta/tests/__init__.py
+++ ta/tests/__init__.py
@@ -2,4 +2,5 @@ from ta.tests.pipeline_wrapper import TestTAFeaturesTransform
 from ta.tests.trend import (TestADXIndicator, TestCCIIndicator,
                             TestVortexIndicator)
 from ta.tests.utils import TestGeneral
-from ta.tests.volume import TestOnBalanceVolumeIndicator
+from ta.tests.volume import (TestEaseOfMovementIndicator,
+                             TestOnBalanceVolumeIndicator)
