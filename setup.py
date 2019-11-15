b55eafc47a44ef100200c43a61abdd5b356358ea
Dario Lopez Padial <bukosabino@gmail.com>
2019-11-15 08:56:49 +0100

    update pypi version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'ta',
     packages = ['ta'],
-    version = '0.5.4',
+    version = '0.5.7',
     description='Technical Analysis Library in Python',
     long_description='It is a Technical Analysis library to financial time series datasets. You can use to do feature engineering. It is builded on Python Pandas library.',
     author = 'Dario Lopez Padial (Bukosabino)',
@@ -16,7 +16,7 @@ setup(
         'pandas',
         'scikit-learn'
     ],
-    download_url = 'https://github.com/bukosabino/ta/tarball/0.5.4',
+    download_url = 'https://github.com/bukosabino/ta/tarball/0.5.7',
     keywords = ['technical analysis', 'python3', 'pandas'],
     license='The MIT License (MIT)',
     classifiers = [],
