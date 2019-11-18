5db23094957977090f387bfa7e32c078e4755dde
Dario Lopez Padial <bukosabino@gmail.com>
2019-11-18 19:17:14 +0100

    adding tests to Bollinger Bands indicator

--- ta/tests/__init__.py
+++ ta/tests/__init__.py
@@ -3,7 +3,7 @@ from ta.tests.pipeline_wrapper import TestTAFeaturesTransform
 from ta.tests.trend import (TestADXIndicator, TestCCIIndicator,
                             TestMACDIndicator, TestVortexIndicator)
 from ta.tests.utils import TestGeneral
-from ta.tests.volatility import TestAverageTrueRange
+from ta.tests.volatility import TestAverageTrueRange, TestBollingerBands
 from ta.tests.volume import (TestAccDistIndexIndicator,
                              TestEaseOfMovementIndicator,
                              TestForceIndexIndicator,
