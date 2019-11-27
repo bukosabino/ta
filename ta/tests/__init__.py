1a083935a34e26806cd67ab8886e2adfd3890846
Dario Lopez Padial <bukosabino@gmail.com>
2019-11-27 10:03:57 +0100

    delete pipeline transforme test

--- ta/tests/__init__.py
+++ ta/tests/__init__.py
@@ -1,4 +1,4 @@
-from ta.tests.pipeline_wrapper import TestTAFeaturesTransform
+# from ta.tests.pipeline_wrapper import TestTAFeaturesTransform
 from ta.tests.trend import (TestADXIndicator, TestCCIIndicator,
                             TestMACDIndicator, TestPSARIndicator,
                             TestVortexIndicator)
