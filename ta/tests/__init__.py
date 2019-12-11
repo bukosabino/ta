37a523052819c1f39fec455fd67dd2dd8327d264
Dario Lopez Padial <bukosabino@gmail.com>
2019-12-11 12:46:35 +0100

    include tests

--- ta/tests/__init__.py
+++ ta/tests/__init__.py
@@ -5,7 +5,7 @@ from ta.tests.momentum import (TestKAMAIndicator, TestMFIIndicator,
 from ta.tests.trend import (TestADXIndicator, TestCCIIndicator,
                             TestMACDIndicator, TestPSARIndicator,
                             TestVortexIndicator)
-# from ta.tests.utils import TestGeneral
+from ta.tests.utils import TestGeneral
 from ta.tests.volatility import TestAverageTrueRange, TestBollingerBands
 from ta.tests.volume import (TestAccDistIndexIndicator,
                              TestEaseOfMovementIndicator,
